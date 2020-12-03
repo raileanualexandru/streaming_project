29a4e1e54021c9ac6c0d1563288cc55f71ee8e8a
gema <gema.mugore@gmail.com>
2020-12-03 09:29:15 -0500

    pull

--- footer.php
+++ footer.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Footer</title>
 </head>
 <body>
     
 </body>
 </html>
+
+
+<?php
+
+
+?>
